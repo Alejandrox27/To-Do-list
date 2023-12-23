56d82c4d9ad3833e5ba202ce7f2ff677631fb6f0
alx_27 <didieralejandromejia83@gmail.com>
2023-12-23 14:53:03 -0500

    Class for messages and import

---
+++ script.js
@@ -0,0 +1,5 @@
+import ToDo from "./ToDoClass.js";
+
+const list = document.getElementById("toDoList");
+const template = document.getElementById("todotemplate");
+const addButton = document.getElementById("add");
